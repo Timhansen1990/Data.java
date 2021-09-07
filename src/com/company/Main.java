ba0b3fc14c00983a1904f31870b1289f055c142a
Timhansen1990 <timx1776@edu.easj.dk>
2021-09-07 10:58:20 +0200

    Data.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String day = "Tuesday";
+        int date = 7;
+        String Month = "September";
+        int year = 2021;
 
-        System.out.println("Hello World!");
+
+        System.out.print("American format: ");
+        System.out.print(day);
+        System.out.print(", ");
+        System.out.print(Month);
+        System.out.print(" ");
+        System.out.print(date);
+        System.out.print(", ");
+        System.out.print(year);
     }
 }
